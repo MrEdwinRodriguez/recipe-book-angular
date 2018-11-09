0573a8a06aea67fcd6e8d1a432fd7191a4c4816d
Edwin <mredwinrodriguez@gmail.com>
2018-11-08 22:05:32 -0500

    fixed recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipes.service';
 
@@ -8,7 +8,7 @@ import { RecipeService } from '../recipes.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService) { 
@@ -19,8 +19,6 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe)
-  }
+
 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,9 +1,14 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+// import { Ingredient } from '../shared/Ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
+    
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', "This is a test desc", 'https://image.shutterstock.com/z/stock-photo-fried-salmon-steak-with-fresh-vegetables-salad-feta-cheese-concept-healthy-food-551147557.jpg'), 
-        new Recipe('The Second Recipe', "This is a another descript", 'https://image.shutterstock.com/z/stock-photo-fried-salmon-steak-with-fresh-vegetables-salad-feta-cheese-concept-healthy-food-551147557.jpg') 
+        new Recipe('A Test Recipe', "This is a test desc", 'https://image.shutterstock.com/z/stock-photo-fried-salmon-steak-with-fresh-vegetables-salad-feta-cheese-concept-healthy-food-551147557.jpg', [ new Ingredient('Meat', 1), new Ingredient('Fries', 30) ]), 
+        new Recipe('Hod Dog', "This is a another descript", 'https://image.shutterstock.com/z/stock-photo-fried-salmon-steak-with-fresh-vegetables-salad-feta-cheese-concept-healthy-food-551147557.jpg', [ new Ingredient('Hot Dog', 30), new Ingredient('Buns', 30) ]) 
       ];
     
       getRecipes() {
